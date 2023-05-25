d28616f55aed3bcec2860afd771b4d0f01e9f3cb
ParthivPatel7 <pate4896@sheridancollege.ca>
2023-05-25 14:31:21 -0400

    Week 3 Project Commited first

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ppart
+ */
+public class Main {
+ /*Push -> Steps, 1) Intialize versioning, 3) .gitconfig file, 3) .gitignore file, local repositorey, commit*/
+
+public static void main(String[] args){
+    System.out.println("Shashvi");
+}
+    
+}
+
