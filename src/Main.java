ed46328dc9257e062ea862c47e0d8e8e098e9f9e
ParthivPatel7 <pate4896@sheridancollege.ca>
2023-05-25 15:26:12 -0400

    New Commit with branch comments

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,18 @@
  */
 public class Main {
  /*Push -> Steps, 1) Intialize versioning, 3) .gitconfig file, 3) .gitignore file, local repositorey, commit*/
+    /*Pull -> Pehla Fetch, pachi check, and pachi merge karvanu*/
 
 public static void main(String[] args){
     System.out.println("Shashvi");
         System.out.println("Loves Me");
+        
+        /*Have ama mare pull karvu hoy to directly pull karis to ekdam j remote repo par change thai jase pan fetch and merge karis to directly nai thay pan ek copy banine avse jenathi 
+        hu code ne commit karis and push karis to remote par nai change thai jay*/
 }
+
     
 }
 
+
+/*Branch ma koi bi file par yellow symbol hoy to ae signify kare ke ae code netbeams ma */
